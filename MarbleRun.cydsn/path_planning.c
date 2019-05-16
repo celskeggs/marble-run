3ef2ef6747c789cd38ace7169dafa45189ab9c40
Cel Skeggs <cela@mit.edu>
2019-05-16 13:54:39 -0400

    Fix numbers slightly

--- MarbleRun.cydsn/path_planning.c
+++ MarbleRun.cydsn/path_planning.c
@@ -22,7 +22,7 @@ struct servo_point graph2[] = {{0, -70, 175, -40}}; // drop waypoint
 struct servo_point graph3[] = {{0, -70, 130, -40}}; // drop waypoint
 struct servo_point graph4[] = {{0, -70, 30, -40}}; // drop waypoint
 struct servo_point graph5[] = {{0, -70, -50, 165}}; // pickup waypoint
-struct servo_point graph6[] = {{30, -60, 30, 175}, {0, -60, 30, 175}, {-20, -60, 30, 175}}; // pickup positions
+struct servo_point graph6[] = {{30, -60, 30, 175}, {5, -60, 30, 175}, {-20, -60, 30, 175}}; // pickup positions
 struct servo_point *graph[] = {graph1, graph2, graph3, graph4, graph5, graph6};
 // DEBUG at T3121707   : uart(OVR=false) servo_control(neglim=120 poslim=180 lwrite=(S 0.0000 G-70.00 L-50.00 R165.00)) control_loop(PM=-unowned RUNFQ=20.00 ERRMX=false MAXSV=(s 40.000 g150.00 l70.000 r130.00) MAXHV=(s 360.00 g300.00 l300.00 r300.00) +{ DPOS=(S 0.0000 G-70.00 L-50.00 R165.00) IPOS=(S 0.0000 G-70.00 L-50.00 R165.00) TPOS=(S 0.0000 G-70.00 L-50.00 R165.00) ATP=true  SM=false ATPN=0   *+) sensor_driver(MCM=-unowned ERRMX=false D0=false D1=false D2=false S0=1067     S1=133      S2=478      +{ MC=0 SLD=false MDA=T1858436    MCN=0   *+) path_planning(W1=(S 0.0000 G-70.00 L175.00 R60.000) W2=(S 0.0000 G-70.00 L175.00 R-40.00) W3=(S 0.0000 G-70.00 L130.00 R-40.00) W4=(S 0.0000 G-70.00 L30.000 R-40.00) W5=(S 0.0000 G-70.00 L-50.00 R165.00) W60=(S 30.000 G-60.00 L30.000 R175.00) W61=(S 0.0000 G-60.00 L30.000 R175.00) W62=(S -20.00 G-60.00 L30.000 R175.00)) sequencer(drop=0.500 pickup=2.000 stop=false)
 
